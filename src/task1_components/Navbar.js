b5b3ffe3de9f1d1b4dfdd5b6494dbeda951289ae
gyanoday kothari <gyanoday06@gmail.com>
2023-09-28 08:00:57 +0530

    done updating

--- src/task1_components/Navbar.js
+++ src/task1_components/Navbar.js
@@ -12,10 +12,10 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="./">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">{props.about}</a>
+              <a className="nav-link" href="./">{props.about}</a>
             </li>
           </ul>
         </div>
