bf226fc7b74a17383723e5ff82e14911ca196139
gyanoday kothari <gyanoday06@gmail.com>
2023-10-14 19:00:12 +0530

    updated tasks

---
+++ src/tasks/Task6.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+function Task6() {
+  return (
+    <div className="portfolio">
+      <header>
+        <img src="https://i.imgur.com/TkfcYa9.jpg" alt="" className='portfolio--img'/>
+        <h1>Gyanoday Kothari</h1>
+        <p>December 06, 2003</p>
+        <p>Email: gyanoday06@gmail.com</p>
+        <p>Phone: 9025667712</p>
+        <p>Website: <a href="https://gyanoday06.netlify.app">gyanoday06.netlify.app</a></p>
+      </header>
+        <br />
+      <h2>Education</h2>
+      <p>Vellore Institute of Technology</p>
+      <p>B.Tech CSE</p>
+      <p>2021-2025</p>
+
+      <p>
+        In my third year as a software engineering student, I'm fully invested in
+        expanding my knowledge. I'm actively participating in various courses,
+        focusing on improving my programming abilities and problem-solving skills.
+      </p>
+        <br />
+      <h2>Web Development Skills</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>DBMS</li>
+        <li>PHP</li>
+        <li>Node.JS</li>
+        <li>React</li>
+      </ul>
+
+      <h2>Languages</h2>
+      <ul>
+        <li>C/C++</li>
+        <li>Java</li>
+        <li>Python</li>
+      </ul>
+
+      <h2>Projects</h2>
+      <ol>
+        <li>Space-Tour - <a href="https://gyanoday06.github.io/space-tour/">View Project</a></li>
+        <li>PGLife - <a href="http://pglifegk.free.nf/">View Project</a></li>
+        <li>RepLap - <a href="https://codepen.io/gyanoday06/full/qBQdqoo">View Project</a></li>
+      </ol>
+
+      <h2>Certifications</h2>
+      <ol>
+        <li>Full Stack - <a href="https://trainings.internshala.com/view_certificate/a5wcycn2h07/ikiyp7snhlx/">View Certificate</a></li>
+        <li>AWS - <a href="https://credly.com/badges/b9e4a88a-6642-406b-9c32-a682b4501826/">View Certificate</a></li>
+        <li>Python Bootcamp - <a href="https://www.udemy.com/certificate/UC-63fe4599-e76d-472d-a827-6c1e63ca46a5/">View Certificate</a></li>
+      </ol>
+
+      <h2>Soft Skills</h2>
+      <ul>
+        <li>Teamwork</li>
+        <li>Time Management</li>
+        <li>Leadership</li>
+        <li>Positive Attitude</li>
+      </ul>
+      <br />
+    </div>
+  );
+}
+
+export default Task6;
